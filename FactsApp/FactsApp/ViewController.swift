d43fe69d97b11091cdcb37a487234a09c020f7a6
mable <mable@qburst.com>
2020-03-30 12:38:21 +0530

    Initial project setup

---
+++ FactsApp/FactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactsApp
+//
+//  Created by Mable on 30/03/20.
+//  Copyright © 2020 Mable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
